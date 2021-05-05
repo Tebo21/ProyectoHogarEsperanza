12913ee40602f2d1ee992c9f9066b06beaccb5c9
jess1998 <jesicaalvares2@hotmail.com>
2021-05-05 00:34:59 -0500

    ActividadPersona

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/centro_medico/list/list.component';
+import { ActividadPersonaComponent } from './components/actividad-persona/actividad-persona.component';
 
 
 const routes: Routes = [
   { path:'centromedico-listar', component: ListComponent },
+  { path:'actividades', component: ActividadPersonaComponent }
 ];
 
 @NgModule({
