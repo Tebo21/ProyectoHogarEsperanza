8b2ad6914b903f51860aa2a76b9197f162e4ab5f
Alvaro-afk-25 <alvaro.guaman.est@tecazuay.edu.ec>
2021-05-30 09:58:55 -0500

    xd xd xd

--- src/app/components/actividad-persona/crear-actividad/crear-actividad.component.ts
+++ src/app/components/actividad-persona/crear-actividad/crear-actividad.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actividades } from 'src/app/models/Actividades';
-import { TipoActividad } from 'src/app/models/TipoActividad';
 import { ActividadesService } from 'src/app/services/actividades.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModalWindow } from '@ng-bootstrap/ng-bootstrap/modal/modal-window';
@@ -9,39 +8,50 @@ import { NgbModalWindow } from '@ng-bootstrap/ng-bootstrap/modal/modal-window';
 @Component({
   selector: 'app-crear-actividad',
   templateUrl: './crear-actividad.component.html',
-  styleUrls: ['./crear-actividad.component.css']
+  styleUrls: ['./crear-actividad.component.css'],
 })
 export class CrearActividadComponent implements OnInit {
-  @Input() modal: NgbModalWindow 
-  public fecha: Date =  new Date();
-  public tipo:Array<string> =  new Array();
-  public actividadCreate: Actividades = new Actividades(0,"",this.fecha,"",this.tipo);
-  public id : number;
+  @Input() modal: NgbModalWindow;
+  public fecha: Date = new Date();
+  public tipo: Array<string> = new Array();
+  public actividadCreate: Actividades = new Actividades(
+    0,
+    '',
+    this.fecha,
+    '',
+    this.tipo
+  );
+  public id: number;
 
-  constructor(private router: Router, public _actividadservice: ActividadesService,private modalService: NgbModal) { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private router: Router,
+    public _actividadservice: ActividadesService,
+    private modalService: NgbModal
+  ) {}
+  ngOnInit(): void {}
   addActividad(): void {
     var d = new Date();
     this.fecha.getUTCDate;
-    this.id=(d.getDate()*d.getDay()*d.getFullYear()*d.getHours()*d.getSeconds());
+    this.id =
+      d.getDate() *
+      d.getDay() *
+      d.getFullYear() *
+      d.getHours() *
+      d.getSeconds();
     console.log(this.id);
-    this.actividadCreate.idActividadPersona =this.id;
-    console.log(this.actividadCreate)
-    this._actividadservice.createActividad(this.actividadCreate).subscribe(res=> {
-      console.log(res);
-    }, err=> {
-      console.log(err);
-    })
+    this.actividadCreate.idActividadPersona = this.id;
+    console.log(this.actividadCreate);
+    this._actividadservice.createActividad(this.actividadCreate).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
     this.gotoList();
   }
   gotoList() {
     this.router.navigate(['/actividades']);
   }
-
-
-
-
-
 }
