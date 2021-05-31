f7ff7f90b10f615011aa1bfc14498097dbc3716f
Alvaro-afk-25 <alvaro.guaman.est@tecazuay.edu.ec>
2021-05-30 20:31:24 -0500

    Heee o

--- src/app/components/actividad-persona/actividad-persona.component.ts
+++ src/app/components/actividad-persona/actividad-persona.component.ts
@@ -1,19 +1,26 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActividadesService } from 'src/app/services/actividades.service';
-import { CrearActividadComponent } from './crear-actividad/crear-actividad.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
+import { PersonasService } from 'src/app/services/personas.service';
+import { Personas } from 'src/app/models/personas';
 
 @Component({
   selector: 'app-actividad-persona',
   templateUrl: './actividad-persona.component.html',
-  styleUrls: ['./actividad-persona.component.css']
+  styleUrls: ['./actividad-persona.component.css'],
 })
 export class ActividadPersonaComponent implements OnInit {
-  
   catalogoId: number = 0;
-  constructor(private router: Router,public _actividadservice: ActividadesService,public modalService: NgbModal) { }
+  PersonAsId: Personas[]=[];
+  PersonId: string;
+
+  constructor(
+    private router: Router,
+    public _actividadservice: ActividadesService,
+    public modalService: NgbModal,
+    public personaService: PersonasService
+  ) {}
 
   ngOnInit(): void {
     this.mostrarActividades();
@@ -21,19 +28,30 @@ export class ActividadPersonaComponent implements OnInit {
 
   mostrarActividades(): void {
     this._actividadservice.getAll().subscribe(
-      response => {
-       this._actividadservice.actividades=response;
+      (response) => {
+        this._actividadservice.actividades = response;
 
         console.log(response);
       },
-      error => {
+      (error) => {
         console.log(error);
       }
-    )
+    );
+  }
+
+  getPersonsById(): void {
+    this.personaService.getPorCedula(this.PersonId).subscribe(
+      (response) => {
+        this.PersonAsId = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   gotoList() {
     this.router.navigate(['/crear-actividad']);
   }
-
 }
