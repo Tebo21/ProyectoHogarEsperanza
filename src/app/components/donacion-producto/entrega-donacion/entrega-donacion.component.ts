015ff7305f56995fcaad5ef35f5c7b53469cad12
lidiacon-9911 <prus1bcclm99@gmail.com>
2021-06-03 13:06:53 -0500

    entrega donacion

--- src/app/components/donacion-producto/entrega-donacion/entrega-donacion.component.ts
+++ src/app/components/donacion-producto/entrega-donacion/entrega-donacion.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Donaciones } from 'src/app/models/Donaciones';
 import { FichaSocioeconomica } from 'src/app/models/ficha-socioeconomica';
 import { FichaSocioeconomicaService } from 'src/app/services/ficha-socioeconomica.service';
 import { PersonasService } from 'src/app/services/personas.service';
+import { DonaProductoService } from '../../../services/dona-producto.service';
 
 @Component({
   selector: 'app-entrega-donacion',
@@ -11,15 +13,28 @@ import { PersonasService } from 'src/app/services/personas.service';
 export class EntregaDonacionComponent implements OnInit {
   donacionProd: FichaSocioeconomica={};
 
+  listaDonaciones: Array<Donaciones> = [];
+
   cedulaPersona: string = '';
 
-  nombreBeneficiado: string = '';
-  correoBeneficiado: string = '';
-  telefonoBeneficiado: string = '';
+  nombreBeneficiario: string = '';
+  edadBeneficiario: number ;
+  direccionBeneficiario: string ='';
+  estadoCivil: string = '';
+//Modals
+  alerta: string;
+  display: boolean;
+  displayF: boolean;
 
-  constructor(private personaService: PersonasService, fichaSer:FichaSocioeconomicaService) { }
+
+  constructor(
+    private personaService: PersonasService, 
+    private fichaSer:FichaSocioeconomicaService,
+    private donacionService: DonaProductoService
+    ) { }
 
   ngOnInit(): void {
+    this.obtenerDonaciones();
   }
   
 
@@ -41,11 +56,40 @@ export class EntregaDonacionComponent implements OnInit {
   buscarPersona(){      
       this.personaService.getPorCedula(this.cedulaPersona).subscribe(
         data => {
-          this.nombreBeneficiado = data.nombres + ' ' + data.apellidos;
-          this.correoBeneficiado = data.correo;
-          this.telefonoBeneficiado = data.celular;
+          this.nombreBeneficiario = data.nombres + ' ' + data.apellidos;
+          this.edadBeneficiario = data.edad;
+          this.direccionBeneficiario = data.direccion;
+          this.estadoCivil = data.estado_civil;
         }) 
       
+    }
+
+
+    obtenerDonaciones(){
+      this.donacionService.getDonaciones().subscribe(
+        data => {
+          this.listaDonaciones = data.map(
+            result => {
+              let donacion = new Donaciones;
+  
+              donacion.idDonacion = result.idDonacion;
+              donacion.cantidad =  result.cantidad;
+              donacion.categoria = result.categoria;
+              donacion.cedulaPersona = result.cedulaPersona;
+              donacion.descripcionDonacion = result.descripcionDonacion;
+              donacion.fechaDonacion = result.fechaDonacion;
+              donacion.nombreDonacion = result.nombreDonacion;
+              
+              return donacion;
+            }
+          )        
+        }
+      )
+    } 
+    donar(){ 
+      this.alerta ="Cantidad a donar";      
+      this.display = true;     
+
     }
   
 
