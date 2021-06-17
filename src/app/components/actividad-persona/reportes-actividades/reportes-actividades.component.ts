6f6a26246f83deb62d32f673f8f59b243744eeee
Jessica Alvarez <jesicaalvares2@hotmail.com>
2021-06-16 21:33:55 -0500

    vista reportes

--- src/app/components/actividad-persona/reportes-actividades/reportes-actividades.component.ts
+++ src/app/components/actividad-persona/reportes-actividades/reportes-actividades.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reportes-actividades.component.css']
 })
 export class ReportesActividadesComponent implements OnInit {
-
+busquedaSeleccion : number;
+selectBusqueda: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.validarVista;
   }
 
+  validarVista(){
+    this.selectBusqueda = this.busquedaSeleccion;
+    console.log(this.selectBusqueda);
+  }
 }
